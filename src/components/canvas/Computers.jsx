bdf5b7c647b8fe3dd70afae33560f8ff024cd43b
Geetesh Gupta <geeteshgupta98@gmail.com>
2023-04-10 23:42:52 +0530

    Update to render correctly on mobile

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,7 +9,7 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
+      <hemisphereLight intensity={0.15} groundColor="black" />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -53,9 +53,11 @@ const ComputersCanvas = () => {
     };
   }, []);
 
+  if (isMobile) return <></>;
+
   return (
     <Canvas
-      frameloop='demand'
+      frameloop="demand"
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
